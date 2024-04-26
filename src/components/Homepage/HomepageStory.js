bed5dff72c8b15cbf52ba5a3f4600e67eccfff91
lvcy <lvcyferrous@gmail.com>
2024-04-26 14:19:27 -0600

    Implemented HomepageStory

--- src/components/Homepage/HomepageStory.js
+++ src/components/Homepage/HomepageStory.js
@@ -2,29 +2,23 @@ import { forwardRef } from 'react'
 import skeleIconLeft from '../../img/shapes/skele_icon_left.png'
 import skeleIconMiddle from '../../img/shapes/skele_icon_middle.png'
 import skeleIconRight from '../../img/shapes/skele_icon_right.png'
-// import 
+import barBreaker from '../../img/shapes/bar_breaker.png'
 
 export const HomepageStory = forwardRef((_, ref) => {
-  const placeholderStyles = {
-    height: "1000px",
-    width: "100%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+  const storyParagraphA = "Peace of Mind Spine (POMS) provides a streamlined online platform for patients to receive unbiased evaluations of their spinal conditions. Our aim is to empower patients with knowledge about their treatment options. Led by Dr. Cary R. Templin, a board-certified spinal surgeon with 18 years of experience, our philosophy prioritizes minimally invasive procedures to enhance patients' quality of life."
 
-  // const story = "Peace of Mind Spine (POMS) provides a streamlined for "
+  const storyParagraphB = "Dr. Templin, who has undergone successful spinal fusion surgery himself, empathizes with patients' pain and disabilities. At POMS, we believe in offering patients independent opinions on their medical care, free from any financial influences. Our mission is to ensure that each patient receives appropriate treament trailored to optimize their quality of life."
 
   return (
-    <section className="HomepageStory" ref={ref} style={placeholderStyles}>
-      {/* <h1>Our Story</h1>
-      <h4>Coming soon!</h4> */}
+    <section className="HomepageStory" ref={ref}>
       <div className="--icon-container">
-        <img src={skeleIconLeft} alt="Skeleton left" />
-        <img src={skeleIconMiddle} alt="Skeleton middle" />
-        <img src={skeleIconRight} alt="Skeleton right" />
+        <img src={skeleIconLeft} alt="Skeleton left" className="smaller" />
+        <img src={skeleIconMiddle} alt="Skeleton middle" className="larger" />
+        <img src={skeleIconRight} alt="Skeleton right" className="smaller" />
       </div>
+      <p className="--story-content">{storyParagraphA}</p>
+      <img src={barBreaker} alt="Paragraph break in blocks of colors" />
+      <p className="--story-content">{storyParagraphB}</p>
     </section>
   )
 })
