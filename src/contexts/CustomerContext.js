1fa446419702ebe9f5296dd0da1855d377228f06
lvcy <lvcyferrous@gmail.com>
2024-07-20 15:41:47 -0600

    Put back form validation

--- src/contexts/CustomerContext.js
+++ src/contexts/CustomerContext.js
@@ -147,7 +147,7 @@ const CustomerProvider = ({ children }) => {
     setStepsCompleted(_stepsCompleted);
   };
   useEffect(() => {
-    // determineStepsCompleted()
+    determineStepsCompleted()
   }, [customerProfile, pane])
 
   const resetForm = () => {
