e4195bef1550bf3980a976448e1bc59c61b030fd
lvcy <lucy.saetehn@gmail.com>
2024-10-15 13:15:21 -0600

    addition+test: Anatomy component with placeholder <iframe>

--- src/components/Assorted/Anatomy.js
+++ src/components/Assorted/Anatomy.js
@@ -1,3 +1,15 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 export const Anatomy = () => {
-  return <section className="Anatomy"></section>
+  return <section className="Anatomy">
+    <iframe src="https://www.ypo.education/csc-MjMzOTY0OA==/s-16/c-14/" 
+      id="ypoedu-container" 
+      name="ypoedu-container" width="100%" 
+      border="0px" 
+      frameborder="0px" 
+      style={{
+        height: "300px",
+        width: "100%"
+      }} 
+    allowfullscreen="allowfullscreen"></iframe><script src="https://www.ypo.education/js/ypo-edu-embed.js"></script>
+  </section>
 }
