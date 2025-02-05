9395340bfa6f7e975e1abc878d7f1badb7afefb2
lvcy <lucy.saetehn@gmail.com>
2025-02-04 22:51:36 -0700

    change: removed .env dependent var

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const Axios = axios.create({
-  baseURL: process.env.REACT_APP_BASE_URL,
+  baseURL: "http://localhost:3001",
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json'
